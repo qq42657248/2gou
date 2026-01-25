103aaddb5735059ee9365df5bd1389026d1b4313
RT622 <127203903+RT622@users.noreply.github.com>
2026-01-26 04:28:04 +0800

    Update sidebar.tsx

--- src/app/write/components/sidebar.tsx
+++ src/app/write/components/sidebar.tsx
@@ -1,6 +1,7 @@
 import { CoverSection } from './sections/cover-section'
 import { MetaSection } from './sections/meta-section'
 import { ImagesSection } from './sections/images-section'
+import { DownloadSection } from './sections/download-section'
 import { ANIMATION_DELAY, INIT_DELAY } from '@/consts'
 
 export function WriteSidebar() {
@@ -9,6 +10,7 @@ export function WriteSidebar() {
 			<CoverSection delay={INIT_DELAY + ANIMATION_DELAY * 0} />
 			<MetaSection delay={INIT_DELAY + ANIMATION_DELAY * 1} />
 			<ImagesSection delay={INIT_DELAY + ANIMATION_DELAY * 2} />
+			<DownloadSection delay={INIT_DELAY + ANIMATION_DELAY * 3} />
 		</div>
 	)
 }
